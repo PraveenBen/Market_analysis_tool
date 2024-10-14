9c979fae72964455c1d55c33f2a36ee4fd0da0d7
Praveen T <49677734+PraveenBen@users.noreply.github.com>
2024-10-14 18:50:15 +0530

    database file

---
+++ users1.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 14, 2024 at 03:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `market_analysis_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users1`
+--
+
+CREATE TABLE `users1` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `phone` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users1`
+--
+
+INSERT INTO `users1` (`name`, `email`, `password`, `phone`) VALUES
+('praveen', 'praveenbenakannanavar@gmail.com', 'Praveen@123', '9535055419');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
